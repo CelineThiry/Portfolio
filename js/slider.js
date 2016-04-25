820f6101b191db1e64d2f87f6ed7268d8bef1d32
Céline Thiry <thiry.celi@gmail.com>
2016-04-25 15:47:28 +0200

    Worked a lot ! Well done :)

--- js/slider.js
+++ js/slider.js
@@ -3,9 +3,9 @@ var slide = null;
 
 $(function() {
 
-    var ul = $(".slider ul");
-    var ul_texttop = $(".slider-text-top ul");
-    var ul_textbottom = $(".slider-text-bottom ul");
+    var ul = $(".slider-img ul");
+    var ul_text = $(".slider-text ul");
+
 
     var slide_count = ul.children().length;
     var slide_width_pc = 100.0 / slide_count;
@@ -15,16 +15,13 @@ $(function() {
     var first_slide = ul.find("li:first-child");
     var last_slide = ul.find("li:last-child");
 
-    var first_slide_texttop = ul_texttop.find("li:first-child");
-    var last_slide_texttop = ul_texttop.find("li:last-child");
+    var first_slide_text = ul_text.find("li:first-child");
+    var last_slide_text = ul_text.find("li:last-child");
 
-    var first_slide_textbottom = ul_textbottom.find("li:first-child");
-    var last_slide_textbottom = ul_textbottom.find("li:last-child");
 
     // Clone the last slide and add as first li element :)
     last_slide.clone().prependTo(ul);
-    last_slide_texttop.clone().prependTo(ul_texttop);
-    last_slide_textbottom.clone().prependTo(ul_textbottom);
+    last_slide_text.clone().prependTo(ul_text);
 
     //Ajout des bullets en dessous du slider
     var bullets_html = '<ul>';
@@ -38,12 +35,12 @@ $(function() {
 
     // Clone the first slide and add as last li element
     first_slide.clone().appendTo(ul);
-    first_slide_texttop.clone().appendTo(ul_texttop);
-    first_slide_textbottom.clone().appendTo(ul_textbottom);
+    first_slide_text.clone().appendTo(ul_text);
+   
 
     ul.css("margin-left", "-100%");
-    ul_texttop.css("margin-left", "-100%");
-    ul_textbottom.css("margin-left", "-100%");
+    ul_text.css("margin-left", "-100%");
+
 
     ul.find("li").each(function(indx) {
         var left_percent = (slide_width_pc * indx) + "%";
@@ -55,7 +52,7 @@ $(function() {
         });
     });
 
-   ul_texttop.find("li").each(function(indx) {
+   ul_text.find("li").each(function(indx) {
         var left_percent = (slide_width_pc * indx) + "%";
         $(this).css({
             "left": left_percent
@@ -65,15 +62,6 @@ $(function() {
         });
     });
 
-   ul_textbottom.find("li").each(function(indx) {
-        var left_percent = (slide_width_pc * indx) + "%";
-        $(this).css({
-            "left": left_percent
-        });
-        $(this).css({
-            width: (100 / slide_count) + "%"
-        });
-    });
 
     // Listen for click of prev button
     $(".prev").click(function() {
@@ -96,21 +84,19 @@ $(function() {
         var anim_is_done = 0; 
 
         var anim_post_process_interval = setInterval(function() {
-          if(anim_is_done == 3) {
+          if(anim_is_done == 2) {
             clearInterval(anim_post_process_interval);
                         // If new slide is before first slide...
             if (new_slide_index < 0) {
                 ul.css("margin-left", ((slide_count) * (-100)) + "%");
-                ul_texttop.css("margin-left", ((slide_count) * (-100)) + "%");
-                ul_textbottom.css("margin-left", ((slide_count) * (-100)) + "%");
+                ul_text.css("margin-left", ((slide_count) * (-100)) + "%");
                 new_slide_index = slide_count - 1;
                 console.log("main arriere");
             }
             // If new slide is after last slide...
             else if (new_slide_index >= slide_count) {
                 ul.css("margin-left", "-100%");
-                ul_texttop.css("margin-left", "-100%");
-                ul_textbottom.css("margin-left", "-100%");
+                ul_text.css("margin-left", "-100%");
                 new_slide_index = 0;
                 console.log("main avant");
             }
@@ -128,28 +114,21 @@ $(function() {
 
             anim_is_done = anim_is_done + 1;
 
-
         });
 
-      ul_texttop.animate({
-            "margin-left": margin_left_pc
-        }, 400, function() {
-          anim_is_done = anim_is_done + 1;
-
-
-        });
-
-            ul_textbottom.animate({
+      ul_text.animate({
             "margin-left": margin_left_pc
         }, 400, function() {
 
           anim_is_done = anim_is_done + 1;
-
 
         });
 
 
     }
+
+});
+
 /*
     $('.bullet li').on('click', function(event) {
         event.preventDefault();
@@ -160,12 +139,6 @@ $(function() {
 
 */
     // var auto = window.setInterval(slide(), 1000); // 1000 ms = 1 sec
-
-
-
-});
-
-
 
 
 /* var slidecount contient le nombre de slide que j'ai dans mon slideshow
