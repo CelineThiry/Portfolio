ce358e16092afb3ec86b2cdf0e96fc7ea4bd7b27
Céline Thiry <thiry.celi@gmail.com>
2016-06-05 18:13:03 +0200

    Work on responsive

--- js/script.js
+++ js/script.js
@@ -8,14 +8,6 @@ $(function() {
 		$('#sidebar').toggleClass('active');
 	});
 
-	var layer2Slide = $('div.layer2');
-
-	layer2Slide.on('click', function(e){
-		e.preventDefault();
-		console.log('layer2Slide is cliqued');
-		$(this).toggleClass('active');
-	});
-
-
+	
 });
 	
